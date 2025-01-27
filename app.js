ac79a6c30fb44de5af0e9f4b69f9bdb0290faa80
DavidsonVitoriano <133684291+DavidsonVitoriano@users.noreply.github.com>
2025-01-27 16:37:52 -0300

    Desafio Finalizado
    Desenvolvimento completo do código.

---
+++ app.js
@@ -0,0 +1,45 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+
+    let campoAmigo = document.getElementById('amigo');
+    let nomeAmigo = campoAmigo.value;
+
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    // vai adicionar o nome na lista de amigos
+    listaDeAmigos.push(nomeAmigo);
+    atualizarListaDeAmigos();
+    campoAmigo.value = "";
+}
+
+function atualizarListaDeAmigos() {
+    let listaNoHtml = document.getElementById('listaAmigos');
+    listaNoHtml.innerHTML = ""; 
+    //Decidi usar o forEach pra facilitar o retorno da lista //
+    listaDeAmigos.forEach((amigo, index) => {
+    let item = document.createElement('li');
+    item.textContent = amigo;
+    item.className = "nome-item";   
+    listaNoHtml.appendChild(item);
+   });
+}
+
+function sortearAmigo() {
+    // Vai verificar se tem algum nome dentro da lista
+    if (listaDeAmigos.length === 0) {
+        alert("Nenhum nome disponível para sortear.");
+        return;
+    }
+        
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let amigoSorteado = listaDeAmigos[indiceAleatorio];
+    // vai mostrar na tela o nome que foi sorteado    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Amigo sorteado: ${amigoSorteado}</li>`;
+}
